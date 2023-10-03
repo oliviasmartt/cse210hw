a903b000ad80dc0581df422ec74cda7e1d12d67e
oliviasmartt <sma21005@byui.edu>
2023-10-03 12:50:11 -0600

    .net

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,15 +1,15 @@
-    using System;
+using System;
 
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("What is your first name? ");
-            string OSFirst = Console.ReadLine();
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
 
-            Console.Write("What is your last name? ");
-            string OSLast = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
 
-            Console.WriteLine($"Your name is {OSLastName}, {OSFirstName} {OSLastName}.");
-        }
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
+}
